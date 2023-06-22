4196f1085bf97272ad69331d83e580414136eb36
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2023-06-22 17:04:47 +0300

    initial

--- scripts/10_prep_linux_server.sh
+++ scripts/10_prep_linux_server.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 sudo apt update ; sudo apt upgrade -y
-sudo apt install -y python3-pip
-sudo apt install -y python3-venv
-sudo apt install -y git
-sudo apt install -y htop
-sudo apt install -y gcc
+sudo apt install -y python3-pip python3-venv git htop gcc
 
 pip3 install wheel cython
+
+sudo apt install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo service docker restart
 
 
 
--- scripts/30_prep_data.py
+++ scripts/30_prep_data.py
@@ -1,13 +1,64 @@
 #%%
 
-from   dotenv           import load_dotenv
-import pyarrow.parquet as pq
 import os
+import pyarrow         as pa
+import pyarrow.parquet as pq
+
+from   dotenv           import load_dotenv
 # from   IPython.display  import display, HTML, JSON
 
-load_dotenv()
-DATA_PATH = os.getenv("DATA_PATH") 
+def update_year_in_dataframe(df, datetime_col, new_year):
+    # Update year in datetime column
+    df[datetime_col] = df[datetime_col].apply(lambda dt: dt.replace(year=new_year))
+    
+
+def update_year_in_datetime(input_file, output_file, datetime_col, new_year):
+    # Load parquet file into a PyArrow Table
+    table = pq.read_table(input_file)
+
+    # Convert PyArrow Table to pandas DataFrame
+    df = table.to_pandas()
+
+    # Update year in datetime column
+    df[datetime_col] = df[datetime_col].apply(lambda dt: dt.replace(year=new_year))
+
+    # Convert pandas DataFrame back to PyArrow Table
+    updated_table = pa.Table.from_pandas(df)
+
+    # Write updated PyArrow Table to a new parquet file
+    pq.write_table(updated_table, output_file)
 
 
-trips = pq.read_table('trips.parquet')
-trips = trips.to_pandas()
+def format_path(path):
+    return path if path.endswith("/") else path + "/"
+
+
+def main():
+    load_dotenv()
+    DATA_PATH = format_path(os.getenv("DATA_PATH"))
+    # if not os.getenv("DATA_PATH").endswith("/") else os.getenv("DATA_PATH")[:-1]
+
+    input_file = f"{DATA_PATH}yellow_tripdata_2020-04.parquet"
+    print(f"Reading {input_file=}")
+    dTrips = pq.read_table(input_file)
+    dTrips = dTrips.to_pandas()
+
+    print(dTrips.head())
+    print(f"{dTrips.shape=}")
+    # trips.head()
+
+    update_year_in_datetime(input_file, f"{DATA_PATH}/yellow_tripdata_2019-04.parquet", "tpep_pickup_datetime", 2019)
+    # tpep_pickup_datetime
+    
+    input_file = f"{DATA_PATH}yellow_tripdata_2019-04.parquet"
+    print(f"Reading {input_file=}")
+    dTrips = pq.read_table(input_file)
+    dTrips = dTrips.to_pandas()
+
+    print(dTrips.head())
+
+
+if __name__ == "__main__":
+    main()
+
+
