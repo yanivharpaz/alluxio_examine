099ba30e27de93dfd7762c8c2da55c7a0a4b6bac
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2023-06-22 11:20:57 +0300

    initial

---
+++ scripts/10_prep_linux_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update ; sudo apt upgrade -y
+sudo apt install -y python3-pip
+sudo apt install -y python3-venv
+sudo apt install -y git
+sudo apt install -y htop
+sudo apt install -y gcc
+
+pip3 install wheel cython
+
+
+
---
+++ scripts/20_download_dataset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Specify the year and month you want to download
+year=2020
+month=01  # January
+
+
+for year in {2009..2022}; do
+  # Loop over the months
+  for month in {01..12}; do
+    # Construct the download URL
+    url="https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_${year}-${month}.parquet"
+    # Use wget to download the file
+    wget $url
+  done
+done
