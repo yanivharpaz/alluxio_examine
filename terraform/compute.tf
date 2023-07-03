375ba189a9e956e24d3769ea6f4c2214cdd52bd2
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2023-07-04 00:17:45 +0300

    terraform

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -14,12 +14,19 @@ resource "azurerm_windows_virtual_machine" "vm_windows" {
     storage_account_type = "Standard_LRS"
   }
 
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
+    source_image_reference {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "Windows-10"
+    sku       = "win10-21h2-entn"
     version   = "latest"
-  }
+    }
+
+#   source_image_reference {
+#     publisher = "MicrosoftWindowsServer"
+#     offer     = "WindowsServer"
+#     sku       = "2016-Datacenter"
+#     version   = "latest"
+#   }
 
   # provisioner "local-exec" {
   #   command = <<EOH
