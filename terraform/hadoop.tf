d3a5b99429735440b2a57dfd8ae5d48703985919
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2023-07-04 12:28:22 +0300

    terraform

--- terraform/hadoop.tf
+++ terraform/hadoop.tf
@@ -44,7 +44,7 @@ resource "azurerm_hdinsight_hadoop_cluster" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
+  name                     = "stghadoop230704"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
